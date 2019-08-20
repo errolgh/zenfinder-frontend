2a405ed0c15635b22e786b09ac995d5b816b68c5
errolgh <ewatsonrei@gmail.com>
2019-08-20 11:51:14 -0400

    working application

--- src/LocationShow.js
+++ src/LocationShow.js
@@ -26,52 +26,52 @@ class LocationShow extends React.Component {
   render(){
     return(
       <div>
-      {this.state.currentLocation ? (
-        <div>
-          <h2>
-            {this.state.currentLocation.title}
-          </h2>
-          <div className="ui two column grid col-height">
-            <div>
-            {this.state.currentLocation ? (
-              <MapContainer
-                handleHover={this.props.handleHover}
-                currentCenter={this.props.currentCenter}
-                handleUnhover={this.props.handleUnhover}
-                currentPopupObj={this.props.currentPopupObj}
-                handleLocation={this.props.handleLocation}
-                currentLocation={this.state.currentLocation}
-              />) : null}
-            </div>
-            <div>
+        {this.state.currentLocation ? (
+          <div>
+            <h2>
+              {this.state.currentLocation.title}
+            </h2>
+            <div className="ui two column grid col-height">
               <div>
-              <Link to='/reviews/new'
-                currentLocation={this.props.currentLocation}
-              >
-                <button
-                  className="ui button huge primary">
-                  Review this Location
-                </button>
-              </Link>
-            </div>
-          {this.state.currentLocation ? (
+              {this.state.currentLocation ? (
+                <MapContainer
+                  handleHover={this.props.handleHover}
+                  currentCenter={this.props.currentCenter}
+                  handleUnhover={this.props.handleUnhover}
+                  currentPopupObj={this.props.currentPopupObj}
+                  handleLocation={this.props.handleLocation}
+                  currentLocation={this.state.currentLocation}
+                />) : null}
+              </div>
+              <div>
+                <div>
+                <Link to='/reviews/new'
+                  currentLocation={this.props.currentLocation}
+                >
+                  <button
+                    className="ui button huge primary">
+                    Review this Location
+                  </button>
+                </Link>
+                <h2>Reviews</h2>
+              </div>
+              {this.state.currentLocation ? (
 
-            this.state.currentLocation.reviews.map((review) => {
-              return <Review
-              currentLocation={this.state.currentLocation}
-              handleHover={this.props.handleHover}
-              handleUnhover={this.props.handleUnhover}
-              currentPopupObj={this.props.currentPopupObj}
-              handleLocation={this.props.handleLocation}
-              review={review}
-              />
-            })
-          ) : null
-          }
-          </div>
-        </div>
-        </div>
-      ) : null}
+                this.state.currentLocation.reviews.map((review) => {
+                  return <Review
+                  currentLocation={this.state.currentLocation}
+                  handleHover={this.props.handleHover}
+                  handleUnhover={this.props.handleUnhover}
+                  currentPopupObj={this.props.currentPopupObj}
+                  handleLocation={this.props.handleLocation}
+                  review={review}
+                  />
+                })
+              ) : null
+              }
+              </div>
+            </div>
+          </div>) : null}
       </div>
     )
   }
--- src/Review.js
+++ src/Review.js
@@ -2,35 +2,52 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default class Review extends React.Component {
+
+  state = {
+    currentReview: null
+  }
+
+  componentDidMount(){
+    fetch(`http://localhost:3001/reviews/${this.props.review.id}`)
+    .then(r => r.json())
+    .then(reviewObj =>
+      this.setState({
+        currentReview: reviewObj
+      })
+    )
+  }
+
   render(){
     return(
       <React.Fragment>
-        <div class="ui centered card">
-          <div class="image">
-            <img src="http://www.nextfrontierinclusion.org/wp-content/uploads/job-manager-uploads/files/2017/12/user2-160x160.jpg"/>
+        {this.state.currentReview ? (
+          <div class="ui centered card">
+            <div class="image">
+              <img src="http://www.nextfrontierinclusion.org/wp-content/uploads/job-manager-uploads/files/2017/12/user2-160x160.jpg"/>
+            </div>
+            <div class="content">
+              <div class="header">{this.props.review.title}</div>
+                <div class="description">
+                  {this.props.review.description}
+                </div>
+                <div class="meta">
+                <span class="date">"{this.props.review.user_name}"</span>
+                </div>
+            </div>
+            <div className="content">
+                <i className="star icon yellow"></i>
+                {this.props.review.rating}
+            </div>
+            <div className="content">
+              <Link to={`/reviews/${this.state.currentReview.id}`}
+                    className="header">
+              <button className="ui button primary">
+                Details
+              </button>
+              </Link>
+            </div>
           </div>
-          <div class="content">
-            <div class="header">{"Review Title"}</div>
-              <div class="description">
-                {this.props.review.description}
-              </div>
-              <div class="meta">
-              <span class="date">"{this.props.review.user_name}"</span>
-              </div>
-          </div>
-          <div className="content">
-              <i className="star icon yellow"></i>
-              {this.props.review.rating}
-          </div>
-          <div className="content">
-            <Link to='/home'
-                  className="header">
-            <button className="ui button primary">
-              Details
-            </button>
-            </Link>
-          </div>
-        </div>
+        ) : null}
       </React.Fragment>
     )
   }
