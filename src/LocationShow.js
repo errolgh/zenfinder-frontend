18c20d7b57e5f91d73fe553419b4fbb0e57ef0fd
errolgh <ewatsonrei@gmail.com>
2019-08-20 11:05:24 -0400

    mapCentering

--- src/LocationShow.js
+++ src/LocationShow.js
@@ -26,48 +26,52 @@ class LocationShow extends React.Component {
   render(){
     return(
       <div>
-      <h2>
-        {this.props.currentLocation.title}
-      </h2>
-      <div className="ui two column grid col-height">
+      {this.state.currentLocation ? (
         <div>
-        {this.state.currentLocation ? (
-          <MapContainer
-            handleHover={this.props.handleHover}
-            currentCenter={this.props.currentCenter}
-            handleUnhover={this.props.handleUnhover}
-            currentPopupObj={this.props.currentPopupObj}
-            handleLocation={this.props.handleLocation}
-            currentLocation={this.state.currentLocation}
-          />) : null}
-        </div>
-        <div>
-        <div>
-        <Link to='/reviews/new'
-          currentLocation={this.props.currentLocation}
-        >
-          <button
-            className="ui button huge primary">
-            Review this Location
-          </button>
-        </Link>
-        </div>
-        {this.state.currentLocation ? (
+          <h2>
+            {this.state.currentLocation.title}
+          </h2>
+          <div className="ui two column grid col-height">
+            <div>
+            {this.state.currentLocation ? (
+              <MapContainer
+                handleHover={this.props.handleHover}
+                currentCenter={this.props.currentCenter}
+                handleUnhover={this.props.handleUnhover}
+                currentPopupObj={this.props.currentPopupObj}
+                handleLocation={this.props.handleLocation}
+                currentLocation={this.state.currentLocation}
+              />) : null}
+            </div>
+            <div>
+              <div>
+              <Link to='/reviews/new'
+                currentLocation={this.props.currentLocation}
+              >
+                <button
+                  className="ui button huge primary">
+                  Review this Location
+                </button>
+              </Link>
+            </div>
+          {this.state.currentLocation ? (
 
-          this.state.currentLocation.reviews.map((review) => {
-            return <Review
-            currentLocation={this.state.currentLocation}
-            handleHover={this.props.handleHover}
-            handleUnhover={this.props.handleUnhover}
-            currentPopupObj={this.props.currentPopupObj}
-            handleLocation={this.props.handleLocation}
-            review={review}
-            />
-          })
-        ) : null
-        }
+            this.state.currentLocation.reviews.map((review) => {
+              return <Review
+              currentLocation={this.state.currentLocation}
+              handleHover={this.props.handleHover}
+              handleUnhover={this.props.handleUnhover}
+              currentPopupObj={this.props.currentPopupObj}
+              handleLocation={this.props.handleLocation}
+              review={review}
+              />
+            })
+          ) : null
+          }
+          </div>
         </div>
-      </div>
+        </div>
+      ) : null}
       </div>
     )
   }
